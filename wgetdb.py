bfa44eb532e4aa221a48fb19cc12dc4739a588cf
Marica Odagaki <ento.entotto@gmail.com>
2013-12-23 18:02:41 +0900

    Add boilerplate packaging code from cookiecutter https://github.com/sloria/cookiecutter-docopt

--- wgetdb.py
+++ wgetdb.py
@@ -1,12 +1,30 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+'''wgetdb
 
-import argparse
+Usage:
+  wgetdb database_path label url
+  wgetdb -h | --help
+  wgetdb --version
 
-parser = argparse.ArgumentParser(description='簡単な例です') # parserを作る
-parser.add_argument('bar')         # 引数を追加します
-parser.add_argument('-f', '--foo') # オプションを追加します
-parser.add_argument('-r', required=True) # このオプションは必須です
-parser.add_argument('--version', action='version', version='%(prog)s 2.0') # version
-args = parser.parse_args() # コマンドラインの引数を解釈します
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+'''
 
-print(args)
+from __future__ import unicode_literals, print_function
+from docopt import docopt
+
+__version__ = "0.1.0"
+__author__ = "Akira Kozakai"
+__license__ = "MIT"
+
+
+def main():
+    '''Main entry point for the wgetdb CLI.'''
+    args = docopt(__doc__, version=__version__)
+    print(args)
+
+
+if __name__ == '__main__':
+    main()
