801e262090c0500ebd0ec712eedeb8a5a1cab446
Marica Odagaki <ento.entotto@gmail.com>
2013-12-25 11:27:22 +0900

    Conform to pep8

--- wgetdb.py
+++ wgetdb.py
@@ -14,10 +14,10 @@ Options:
 '''
 
 from __future__ import unicode_literals, print_function
-from docopt import docopt
+import datetime
 import urllib2
 import sqlite3
-import datetime
+from docopt import docopt
 
 
 __version__ = "0.1.0"
@@ -31,15 +31,18 @@ URLOPEN_TIME_OUT = 10
 def urldata(url):
     response = urllib2.urlopen(url, timeout=URLOPEN_TIME_OUT)
     if response.code != 200:
-        return None;
+        return None
     return response.read()
+
 
 def add_db(db_path, url, data, label):
     con = sqlite3.connect(db_path, isolation_level=None)
 
     # Create table
-    cur = con.execute("SELECT * FROM sqlite_master WHERE type='table' and name='%s'" % TABLE_NAME)
-    if cur.fetchone() == None:
+    cur = con.execute(
+        "SELECT * FROM sqlite_master WHERE type='table' and name=?",
+        (TABLE_NAME,))
+    if cur.fetchone() is None:
         sql = """
             CREATE TABLE %s (
               data_id INTEGER PRIMARY KEY,
@@ -53,8 +56,10 @@ def add_db(db_path, url, data, label):
         con.execute(sql)
 
     # Add record
-    sql = 'INSERT INTO %s ("url", "label", "data", "created_date") VALUES(?, ?, ?, ?);' % TABLE_NAME
-    cur = con.execute(sql, (url, label, buffer(data), datetime.datetime.utcnow()))
+    sql = ('INSERT INTO %s ("url", "label", "data", "created_date")'
+           'VALUES (?, ?, ?, ?);') % TABLE_NAME
+    args = (url, label, buffer(data), datetime.datetime.utcnow())
+    cur = con.execute(sql, args)
     con.close()
 
 
@@ -71,7 +76,6 @@ def main():
         if data:
             add_db(db_path, url, data, label)
             print('SUCCESS!')
-    
     except Exception as e:
         print(u'=== ERROR ===')
         print(u'type:' + str(type(e)))
